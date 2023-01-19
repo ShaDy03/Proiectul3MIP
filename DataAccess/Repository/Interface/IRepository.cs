06185afa28044e15aa12a3c09a46cf06fca13b2b
Purcia Claudiu <claidiu.purcea031997@gmail.com>
2023-01-20 00:06:59 +0200

    Create few methods in repo

--- DataAccess/Repository/Interface/IRepository.cs
+++ DataAccess/Repository/Interface/IRepository.cs
@@ -1,6 +1,61 @@
-﻿namespace DataAccess.Repository.Interface
+﻿using System.Linq.Expressions;
+
+namespace DataAccess.Repository.Interface
 {
     public interface IRepository<T> where T : class
     {
+        /// <summary>
+        /// Take all results by <typeparamref name="T"/> class.
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <param name="orderBy"></param>
+        /// <param name="includes"></param>
+        /// <returns>IEnumerable<<typeparamref name="T"/>></returns>
+        IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null,
+            Expression<Func<T, object>>? orderBy = null,
+            params Expression<Func<T, object>>[] includes);
+
+        /// <summary>
+        /// Take on <typeparamref name="T"/> class object by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns><typeparamref name="T"/></returns>
+        T GetById(int id);
+
+        /// <summary>
+        /// Take first result by <typeparamref name="T"/> class.
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <param name="orderBy"></param>
+        /// <param name="includes"></param>
+        /// <returns><typeparamref name="T"/></returns>
+        T GetFirstOrDefault(Expression<Func<T, bool>>? filter,
+            params Expression<Func<T, object>>[] includes);
+
+        /// <summary>
+        /// Add in <typeparamref name="T"/> class
+        /// </summary>
+        /// <param name="entity"></param>
+        void Add(T entity);
+
+        /// <summary>
+        /// Delete in <typeparamref name="T"/> class
+        /// </summary>
+        /// <param name="entity"></param>
+        void Remove(T entity);
+
+        /// <summary>
+        /// Return bool if exists or not exists
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        bool Exist(Expression<Func<T, bool>> filter);
+
+        /// <summary>
+        /// Return int count of find in <typeparamref name="T"/> class
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        int Count(Expression<Func<T, bool>>? filter = null);
     }
 }
--- DataAccess/Repository/Repository.cs
+++ DataAccess/Repository/Repository.cs
@@ -1,18 +1,87 @@
 ﻿using DataAccess.Repository.Interface;
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace DataAccess.Repository
 {
     public class Repository<T> : IRepository<T> where T : class
     {
-        protected readonly DbContext Context;
-
         internal DbSet<T> dbSet;
 
         public Repository(DbContext context)
         {
-            Context = context;
             dbSet = context.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null,
+            Expression<Func<T, object>>? orderBy = null,
+            params Expression<Func<T, object>>[] includes)
+        {
+            IQueryable<T> query = dbSet.AsNoTracking();
+            if (filter != null && orderBy == null)
+            {
+                query = query.Where(filter);
+            }
+            else if (filter == null && orderBy != null)
+            {
+                query = query.OrderBy(orderBy);
+            }
+            else if (filter != null && orderBy != null)
+            {
+                query = query.Where(filter).OrderBy(orderBy);
+            }
+            if (includes.Count() > 0)
+            {
+                query = includes.Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
+            }
+            return query.ToList();
+        }
+        public T GetById(int id)
+        {
+            return dbSet.Find(id);
+        }
+        public T GetFirstOrDefault(Expression<Func<T, bool>>? filter,
+            params Expression<Func<T, object>>[] includes)
+        {
+            IQueryable<T> query = dbSet.AsNoTracking();
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            if (includes.Count() > 0)
+            {
+                query = includes.Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
+            }
+            return query.FirstOrDefault();
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public bool Exist(Expression<Func<T, bool>> filter)
+        {
+            if (filter == null)
+                return false;
+            return dbSet.Any(filter);
+        }
+
+        public int Count(Expression<Func<T, bool>>? filter = null)
+        {
+            IQueryable<T> query = dbSet;
+            if (filter != null)
+            {
+                return query.Count(filter);
+            }
+            else
+            {
+                return query.Count();
+            }
         }
     }
 }
